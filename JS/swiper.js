47e6259ba45c2724067951c4ea4c2e029edc78ee
Amaiyo Praises <amaiyo.praises@gmail.com>
2025-08-19 19:37:33 +0100

    Completed the Trusted Partner section

--- JS/swiper.js
+++ JS/swiper.js
@@ -9,3 +9,34 @@ var swiper = new Swiper('.swiper-3', {
     clickable: true,
   },
 });
+
+// var swiper = new Swiper('.mySwiper', {
+//   slidesPerView: 3,
+//   spaceBetween: 10,
+//   //   grid: {
+//   //     rows: 2,
+//   //   },
+//   loop: true,
+//   autoplay: {
+//     delay: 2000,
+//     disableOnInteraction: false,
+//   },
+//   speed: 3000,
+//   pagination: {
+//     el: '.swiper-pagination',
+//     clickable: true,
+//   },
+// });
+
+var swiper = new Swiper('.mySwiper', {
+  slidesPerView: 3,
+  spaceBetween: 5,
+  loop: true,
+  speed: 10000, // bigger = slower continuous speed
+  autoplay: {
+    delay: 0, // no pause
+    disableOnInteraction: false,
+  },
+  freeMode: true,
+  freeModeMomentum: false,
+});
