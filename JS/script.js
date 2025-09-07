f9ad2736faa753e464fb944b97aa4fbfe4f7cb9b
Amaiyo Praises <amaiyo.praises@gmail.com>
2025-09-07 03:10:09 +0100

    Update changes from Preye and general adjustments

--- JS/script.js
+++ JS/script.js
@@ -90,7 +90,7 @@ const particlesContainer = document.getElementById('particles-js');
 
 if (particlesContainer) {
   particlesJS.load('particles-js', '/particlesjs-config.json', function () {
-    console.log('particles.js config loaded');
+    //  console.log('particles.js config loaded');
   });
 }
 
